86b1b68b45e4788cc3e7f5e394ef43b2d79fb852
Felicitas <felifigueroaf@gmail.com>
2023-11-03 11:21:20 +0100

    Initial commit

---
+++ nimbleSurveys/nimbleSurveysApp.swift
@@ -0,0 +1,17 @@
+//
+//  nimbleSurveysApp.swift
+//  nimbleSurveys
+//
+//  Created by Felicitas Figueroa Fagalde on 03/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct nimbleSurveysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
